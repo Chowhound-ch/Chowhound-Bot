746682fbbc95053493d2d86f47a114159180d383
Chowhound <825352674@qq.com>
2023-04-17 20:45:45 +0800

    完善监听器的扫描和注册

---
+++ src/main/kotlin/per/chowhound/bot/mirai/common/MessageAspect.kt
@@ -0,0 +1,9 @@
+package per.chowhound.bot.mirai.common
+
+/**
+ * @Author: Chowhound
+ * @Date: 2023/4/17 - 20:43
+ * @Description: TODO 对监听器的切面
+ */
+class MessageAspect {
+}
--- src/main/kotlin/per/chowhound/bot/mirai/config/MiraiEventListenerConfiguration.kt
+++ src/main/kotlin/per/chowhound/bot/mirai/config/MiraiEventListenerConfiguration.kt
@@ -1,8 +1,11 @@
 package per.chowhound.bot.mirai.config
 
 import cn.hutool.core.util.ClassUtil
+import cn.hutool.extra.spring.SpringUtil
+import jakarta.annotation.PostConstruct
 import net.mamoe.mirai.Bot
 import net.mamoe.mirai.event.Event
+import net.mamoe.mirai.event.EventPriority
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Configuration
 import per.chowhound.bot.mirai.common.utils.Logger.logInfo
@@ -14,22 +17,25 @@ class MiraiEventListenerConfiguration {
     @Autowired
     private lateinit var bot: Bot
 
-    init {
+    @Suppress("UNCHECKED_CAST")
+
+    @PostConstruct
+    fun init() {
         ClassUtil.scanPackage("per.chowhound.bot.mirai").forEach { clazz ->
             clazz.declaredMethods.forEach { method ->
-                method.getAnnotation(Listener::class.java)?.let {
+                method.getAnnotation(Listener::class.java)?.let {listener ->
                     // 扫描所有的 Listener 注解，注册监听器
-                    method.parameterTypes.forEach { parameterType ->
-                        // 如果参数是 Event 的子类，就注册监听器
-                        if (Event::class.java.isAssignableFrom(parameterType)) {
-                            logInfo("注册监听器: ${parameterType.simpleName}")
+                    method.parameterTypes.find { Event::class.java.isAssignableFrom(it) }?.let {
+                        bot.eventChannel.subscribeAlways(it as Class<Event>, priority = listener.priority) {
+                            method.invoke(SpringUtil.getBean(clazz), this)
                         }
+                        logInfo("注册监听器：${clazz.name}.${method.name}")
                     }
                 }
             }
         }
     }
-
 }
-
-annotation class Listener
+@Target(AnnotationTarget.FUNCTION)
+@Retention(AnnotationRetention.RUNTIME)
+annotation class Listener(val priority: EventPriority = EventPriority.NORMAL)
