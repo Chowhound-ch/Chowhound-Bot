f2d56c47718f3ba83e958b52cd0527ea991ecddb
Chowhound <825352674@qq.com>
2023-04-24 22:59:11 +0800

    增加监听函数中发送消息的快捷方法

---
+++ Chowhound-Framework/src/main/kotlin/per/chowhound/bot/mirai/framework/common/MessageSender.kt
@@ -0,0 +1,28 @@
+package per.chowhound.bot.mirai.framework.common
+
+import net.mamoe.mirai.contact.Contact
+import net.mamoe.mirai.event.events.MessageEvent
+import net.mamoe.mirai.message.MessageReceipt
+import net.mamoe.mirai.message.data.*
+
+/**
+ * @Author: Chowhound
+ * @Date: 2023/4/24 - 22:20
+ * @Description:
+ */
+@Suppress("unused")
+object MessageSender {
+
+//    suspend fun GroupMessageEvent.send(message: String): MessageReceipt<Group> = this.subject.sendMessage(message)
+//
+//    suspend fun GroupMessageEvent.send(message: Message): MessageReceipt<Group> = this.subject.sendMessage(message)
+
+    suspend fun MessageEvent.send(message: Message): MessageReceipt<Contact> = this.subject.sendMessage(message)
+
+    suspend fun MessageEvent.send(message: String): MessageReceipt<Contact> = this.subject.sendMessage(message)
+
+    suspend fun MessageEvent.reply(message: MessageChain): MessageReceipt<Contact> = this.send(QuoteReply(message))
+
+    suspend fun MessageEvent.reply(message: String): MessageReceipt<Contact> = this.send(QuoteReply(messageChainOf(PlainText(message))))
+
+}
--- Chowhound-Framework/src/main/kotlin/per/chowhound/bot/mirai/framework/components/state/GroupStateListener.kt
+++ Chowhound-Framework/src/main/kotlin/per/chowhound/bot/mirai/framework/components/state/GroupStateListener.kt
@@ -2,7 +2,9 @@ package per.chowhound.bot.mirai.framework.components.state
 
 import jakarta.annotation.Resource
 import net.mamoe.mirai.event.events.GroupMessageEvent
+import net.mamoe.mirai.message.data.*
 import org.springframework.stereotype.Controller
+import per.chowhound.bot.mirai.framework.common.MessageSender.send
 import per.chowhound.bot.mirai.framework.components.state.service.GroupStateService
 import per.chowhound.bot.mirai.framework.config.Listener
 
@@ -21,5 +23,6 @@ class GroupStateListener {
     @Listener("^/status\\s*{{desState}}")
     suspend fun GroupMessageEvent.updateGroupState(desState: String) {
 
+        send("状态更新成功 $desState")
     }
 }
