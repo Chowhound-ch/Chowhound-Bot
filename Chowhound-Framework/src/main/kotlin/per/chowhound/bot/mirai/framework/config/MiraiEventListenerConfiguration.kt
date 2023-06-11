3f984dff00d178f533ea44916b3f0a7cf3f9e015
Chowhound <825352674@qq.com>
2023-06-11 12:21:55 +0800

    增加持续会话

--- Chowhound-Framework/src/main/kotlin/per/chowhound/bot/mirai/framework/config/MiraiEventListenerConfiguration.kt
+++ Chowhound-Framework/src/main/kotlin/per/chowhound/bot/mirai/framework/config/MiraiEventListenerConfiguration.kt
@@ -1,14 +1,17 @@
+
 package per.chowhound.bot.mirai.framework.config
 
 import cn.hutool.core.util.ClassUtil
 import cn.hutool.extra.spring.SpringUtil
 import jakarta.annotation.PostConstruct
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
+import kotlinx.coroutines.flow.filter
+import kotlinx.coroutines.flow.filterIsInstance
+import kotlinx.coroutines.flow.first
 import net.mamoe.mirai.Bot
 import net.mamoe.mirai.event.Event
 import net.mamoe.mirai.event.EventPriority
+import net.mamoe.mirai.event.GlobalEventChannel
 import net.mamoe.mirai.event.subscribeMessages
 import org.springframework.beans.factory.NoSuchBeanDefinitionException
 import org.springframework.context.annotation.Configuration
@@ -26,6 +29,9 @@ import kotlin.reflect.full.callSuspend
 import kotlin.reflect.full.extensionReceiverParameter
 import kotlin.reflect.jvm.javaMethod
 import kotlin.reflect.jvm.kotlinFunction
+import kotlin.time.Duration
+import kotlin.time.Duration.Companion.seconds
+
 /**
  * @author Chowhound
  * @date   2023/6/8 - 16:45
@@ -33,6 +39,23 @@ import kotlin.reflect.jvm.kotlinFunction
  */
 class ListenerFunctions: MutableMap<Any, MutableList<Pair<KFunction<*>, KClass<out Event>>>> by mutableMapOf()
 
+/**
+ * 持续会话
+ */
+@Suppress("all")
+suspend inline fun  <reified E : Event> E.waitMessage(
+    time: Duration = 15.seconds,
+    noinline filter: (E) -> Boolean = { true }
+): E? {
+    val deferred = CoroutineScope(Dispatchers.IO).async {
+        GlobalEventChannel.asFlow()
+            .filterIsInstance<E>()
+            .filter(filter)
+            .first()
+    }
+
+    return withTimeoutOrNull(time) { deferred.await() }
+}
 
 /**
  * @author Chowhound
@@ -186,9 +209,6 @@ class MiraiEventListenerConfiguration(
     }
 
     fun String.replaceGroupNoCapture() = this.replace("(", "(?:")
-
-
-
 }
 
 
@@ -216,59 +236,43 @@ annotation class Listener(
  *
  * @author ForteScarlet
  */
-enum class MatchType(
-    val isPlainText: Boolean,
-) {
+enum class MatchType{
     /**
      * 全等匹配
-     *
-     * @see StringMatchers.EQUALS
      */
-    TEXT_EQUALS(true),
+    TEXT_EQUALS,
 
     /**
      * 忽略大小写的全等匹配
-     *
-     * @see StringMatchers.EQUALS_IGNORE_CASE
      */
-    TEXT_EQUALS_IGNORE_CASE(true),
+    TEXT_EQUALS_IGNORE_CASE,
 
 
     /**
-     * 首部匹配
-     *
-     * @see StringMatchers.STARTS_WITH
+     * 开头匹配
      */
-    TEXT_STARTS_WITH(true),
+    TEXT_STARTS_WITH,
 
     /**
      * 尾部匹配.
-     *
-     * @see StringMatchers.ENDS_WITH
      */
-    TEXT_ENDS_WITH(true),
+    TEXT_ENDS_WITH,
 
     /**
      * 包含匹配.
-     *
-     * @see StringMatchers.CONTAINS
      */
-    TEXT_CONTAINS(true),
+    TEXT_CONTAINS,
 
     /**
      * 正则完全匹配. `regex.matches(...)`
-     *
-     * @see KeywordRegexMatchers.MATCHES
      */
-    REGEX_MATCHES(false),
+    REGEX_MATCHES,
 
 
     /**
      * 正则包含匹配. `regex.find(...)`
-     *
-     * @see KeywordRegexMatchers.CONTAINS
      */
-    REGEX_CONTAINS(false),
+    REGEX_CONTAINS,
 
     ;
 
