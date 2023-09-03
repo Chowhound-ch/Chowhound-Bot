ad826fb0a9e2544e9beb7d8c5e689b1ab484a7e1
Chowhound <825352674@qq.com>
2023-09-03 21:55:34 +0800

    优化消息事件监听器注册

--- Chowhound-Framework/src/main/kotlin/per/chowhound/bot/mirai/framework/config/listener/utils/PatternUtil.kt
+++ Chowhound-Framework/src/main/kotlin/per/chowhound/bot/mirai/framework/config/listener/utils/PatternUtil.kt
@@ -7,18 +7,13 @@ import net.mamoe.mirai.event.EventPriority
 import net.mamoe.mirai.event.events.FriendMessageEvent
 import net.mamoe.mirai.event.events.GroupMessageEvent
 import net.mamoe.mirai.event.events.MessageEvent
-import org.springframework.context.support.beans
 import org.springframework.core.annotation.AliasFor
 import org.springframework.core.annotation.AnnotationUtils
 import per.chowhound.bot.mirai.framework.components.permit.enums.PermitEnum
 import per.chowhound.bot.mirai.framework.config.EventClassUtil.getIfEvent
 import per.chowhound.bot.mirai.framework.config.MatchType
-import per.chowhound.bot.mirai.framework.config.listener.MiraiListenerConfiguration
 import per.chowhound.bot.mirai.framework.config.listener.utils.EventClassUtil.isMessageEvent
-import per.chowhound.bot.mirai.framework.config.listener.utils.PatternUtil.resolve
-import java.util.Arrays
 import java.util.HashMap
-import java.util.Stack
 import java.util.concurrent.ConcurrentLinkedQueue
 import kotlin.reflect.KClass
 import kotlin.reflect.KFunction
@@ -38,7 +33,7 @@ object PatternUtil {
 
     // `name,\\s+`
     fun Any.resolve(): List<ListenerPatternInfo> {
-        var list = ArrayList<ListenerPatternInfo>()
+        val list = ArrayList<ListenerPatternInfo>()
         this::class.declaredFunctions.forEach { function->
             val listener = AnnotationUtils.getAnnotation(function.javaMethod!!,
                 Listener::class.java) ?: return@forEach
@@ -48,24 +43,29 @@ object PatternUtil {
             val eventClass = function.parameters.stream()
                 .map { it.type.classifier as? KClass<*> }
                 .filter { it != null && it.isSubclassOf(Event::class) }
-                .limit(1)
-                .toList()[0]
+                .limit(1).toList()[0]
             ?: return@forEach
             val event = eventClass.getIfEvent() ?: throw RuntimeException("不是事件类型")
             val info = ListenerPatternInfo(function = function, eventClass = event, bean = this)
 
             if (event.isMessageEvent()){
-                var patternMap = HashMap<String, String>()
-                var regPattern = recur(listener.pattern, patternMap)
+                val patternMap = HashMap<String, String>()
+                val regPattern = recur(listener.pattern, patternMap)
+                info.patternMap = patternMap
+                info.raw = listener.pattern
+                info.regPattern = regPattern
             } else{
                 info.isMessageEvent = false
             }
 
+            list.add(info)
         }
-        return emptyList()
+        return list
     }
 
     /**
+     * <p> 以prefix{{syntax}}suffix为例，其中syntax可为name,pattern或者name
+     *
      * @param str 后半部分的需要替换掉[SEPARATOR_START]包裹的内容的字符串
      * @param groupMap 被替换掉的[SEPARATOR_START]包裹的内容
      *
@@ -76,45 +76,36 @@ object PatternUtil {
      */
     fun recur(str: String, groupMap: MutableMap<String, String>): String{
         val startIndex = str.lastIndexOf(SEPARATOR_START)
-
-        // /name={{name,a{2,14}}}or{{name2,ab{2,14}}}}111
         if (startIndex == -1){ // 没有找到 SEPARATOR_START
             return str
         }
-        // /name={{name,a{2,14}}}or
-        val prefix = str.substring(0, startIndex)
-
-
+        // syntax}}suffix
         val afterStartSign = str.substring(startIndex + SEPARATOR_START.length)
-        val endIndex = afterStartSign.indexOf(SEPARATOR_END)
-
+        // 防止SEPARATOR_END为}}时出现afterStartSign为name,abc{1,2}}}aa}}的情况(正常应匹配aa前的}})
         val charQueue = ConcurrentLinkedQueue(SEPARATOR_END.toCharArray().asList())
-        var i = endIndex + SEPARATOR_END.length
-        while (i < afterStartSign.length){
+        var endIndex = afterStartSign.indexOf(SEPARATOR_END) + SEPARATOR_END.length
+        while (endIndex < afterStartSign.length){
             charQueue.remove()
-            charQueue.add(afterStartSign[i])
-            if (charQueue.joinToString("") != SEPARATOR_END){
-                break
-            }
-            i++
+            charQueue.add(afterStartSign[endIndex])
+            if (charQueue.joinToString("") != SEPARATOR_END){ break }
+            endIndex++
         }
-        val suffix = afterStartSign.substring(i)
-        val pattern = afterStartSign.substring(0, i - SEPARATOR_END.length)
-        val patternPair = pattern.split(',', limit = 2)
+        // ["name", "pattern"]或者pattern未指定默认为["name", ".*?"]
+        val patternPair = afterStartSign.substring(0, endIndex - SEPARATOR_END.length)
+            .split(',', limit = 2)
 
         when(patternPair.size){
             1 -> groupMap[patternPair[0]] = ".*?"
             2 -> groupMap[patternPair[0]] = patternPair[1].replaceGroupNoCapture()
-            else -> {
-                throw RuntimeException("{{name,pattern}}语法错误")
-            }
+            else -> throw RuntimeException("{{name,pattern}}语法错误")
         }
 
-        return recur(prefix, groupMap) + "(?<${patternPair[0]}>${groupMap[patternPair[0]]})" + suffix
+        return recur(str.substring(0, startIndex), groupMap) +
+                "(?<${patternPair[0]}>${groupMap[patternPair[0]]})" +
+                afterStartSign.substring(endIndex)
     }
     // TODO 成对的括号才替换
     fun String.replaceGroupNoCapture() = this.replace("(", "(?:")
-
 
 
     data class ListenerPatternInfo(
@@ -127,10 +118,6 @@ object PatternUtil {
         var isMessageEvent: Boolean?  = true,
         var bean: Any
         )
-}
-
-fun main(args: Array<String>) {
-    PatternUtil.recur("/name={{name,a{2,14}}}or{{name2,ab{2,14}}}111", HashMap())
 }
 
 @Suppress("unused")
